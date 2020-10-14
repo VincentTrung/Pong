eeed136eb2a235bee9aa2ff6e4754f239a5010d4
VincentTrung <60561522+VincentTrung@users.noreply.github.com>
2020-10-14 10:35:41 -0400

    Initial commit

---
+++ Pong.py
@@ -0,0 +1,25 @@
+import turtle
+
+#Basic Setup
+screen = turtle.Screen()            #create screen
+screen.title("PONG")                #Title
+screen.bgcolor('black')             #background
+screen.setup(width=800,height=600)  #Size
+screen.tracer(0)
+
+#Main Game loop
+while True:
+    screen.update()
+
+
+#add game objects to screen (paddle and ball)
+
+#Move paddles
+
+#move ball
+
+#Collision
+
+#Scoring
+
+#sounds
